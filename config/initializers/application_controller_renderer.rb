866aca50eac66fbccfc136560b3168ff299e0c09
bet-better36 <61450126+bet-better36@users.noreply.github.com>
2020-04-28 16:30:19 +0900

    [Finish5-7]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1ad6d4bc6be925a1c0f2946d4fa8dbcb87c9b8638d699db5cc68b734ac5be733fd948da32c684235316db1ffcaeda5a0efb613c6d17c4dd00d8b97300cc3b180'
